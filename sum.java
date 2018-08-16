e593f21f6e38dc3cf5954dd621030dce286691fc
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-16 12:03:45 +0530

    Update and rename README.md to sum.java

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n1,n2,sum=0;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the two integers to cal sum");
+	n1=s.nextInt();
+	n2=s.nextInt();
+	sum=n1+n2;
+	System.out.println("sum of the integers="+sum);
+	
+	}
+}
